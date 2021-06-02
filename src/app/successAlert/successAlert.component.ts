9a261a8ad24dc12a5ba0299126e359ca3d7ed14b
DINESHKUMAR <dk1981999@gmail.com>
2021-06-02 19:30:07 +0530

    modified on assignment

--- src/app/successAlert/successAlert.component.ts
+++ src/app/successAlert/successAlert.component.ts
@@ -5,6 +5,9 @@ selector:'app-success',
 template:`<h5>This is a Success message!</h5>`,
 styles:[`h5{
     color: green;
+    padding: 20px;
+    border: 1px solid green;
+    background-color: salmon;
 }`]
 })
 export class SuccessAlertComponent {
