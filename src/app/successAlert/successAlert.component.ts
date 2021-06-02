dfcd769651f9b0e28a3014339be6bbb44077140b
DINESHKUMAR <dk1981999@gmail.com>
2021-06-02 19:25:30 +0530

    assignment-1

---
+++ src/app/successAlert/successAlert.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+selector:'app-success',
+template:`<h5>This is a Success message!</h5>`,
+styles:[`h5{
+    color: green;
+}`]
+})
+export class SuccessAlertComponent {
+
+} 
