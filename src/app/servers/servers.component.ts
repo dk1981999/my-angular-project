6a38aef7e465c3c49f5ecf1e0db8c0789140192b
DINESHKUMAR <dk1981999@gmail.com>
2021-06-03 12:57:23 +0530

    assignment 3

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -3,24 +3,32 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+  styles:[`.over {
+    color: white;
+  }`]
 })
 export class ServersComponent implements OnInit {
-addNewServer:boolean = false;
-serverCreationStatus: string = "No server was created!";
-serverName:string = "Two Way DataBinding";
-  constructor() {
-    setTimeout(()=>{
-      this.addNewServer = true;
-    },2000)
-   }
+// addNewServer:boolean = false;
+// serverCreationStatus: string = "No server was created!";
+// serverName:string = "Two Way DataBinding";
+showContent:boolean = false;
+logClick= [];
+  // constructor() {
+  //   setTimeout(()=>{
+  //     this.addNewServer = true;
+  //   },2000)
+  //  }
 
   ngOnInit(): void {
   }
-onCreateServer(){
-  this.serverCreationStatus = "Server was created!" + " the server Name is " + this.serverName;
-}
-onUpdateServer(event:Event){
-this.serverName=(<HTMLInputElement>event.target).value;
+// onCreateServer(){
+//   this.serverCreationStatus = "Server was created!" + " the server Name is " + this.serverName;
+// }
+// onUpdateServer(event:Event){
+// this.serverName=(<HTMLInputElement>event.target).value;
+// }
+onToggle(){
+  this.showContent = !this.showContent;
+  return this.logClick.push(this.logClick.length+1);
 }
 }
