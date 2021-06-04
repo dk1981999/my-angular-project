c54c5f08d9a2cdc0150b8a4774e096a944477fd6
DINESHKUMAR <dk1981999@gmail.com>
2021-06-04 15:47:46 +0530

    assignment 4 with reset game option

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  @ViewChild('reset',{static: true}) resetElement: ElementRef;
+  oddNumbers: number[] = [];
+  evenNumbers: number[] = [];
+  onIntervalFired(firedNumber: number){
+    if(firedNumber % 2 == 0){
+      this.evenNumbers.push(firedNumber);
+    }else{
+      this.oddNumbers.push(firedNumber);
+    }
+  }
+  onResetGame(){
+     this.resetElement.nativeElement.innerText='';
+    console.log(this.resetElement)
+  }
 }
