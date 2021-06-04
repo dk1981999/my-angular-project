981bc0a710207b5f86f3bc3e8dcf3116a4765b2d
DINESHKUMAR <dk1981999@gmail.com>
2021-06-04 19:35:12 +0530

    custom directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HighlightDirective } from './highlight.directive';
 import { ServerComponent } from './server/server/server.component';
 import { ServersComponent } from './servers/servers.component';
 
@@ -10,7 +11,8 @@ import { ServersComponent } from './servers/servers.component';
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
