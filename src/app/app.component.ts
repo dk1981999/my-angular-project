2799cd149db6668e60f3bb7a186bfeae874ddc89
DINESHKUMAR <dk1981999@gmail.com>
2021-06-09 14:04:30 +0530

    tdForms completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  defaultValue = "pet";
+  answer:string;
+  genders = ['male','female'];
+  user = {
+    username:'',
+    email:'',
+    secretQuestion:'',
+    answer:'',
+    gender:''
+  }
+  submitted = false;
+  @ViewChild('form') accessForm: NgForm;
   suggestUserName() {
     const suggestedName = 'Superuser';
+    this.accessForm.form.patchValue({
+      userData: {
+        username: suggestedName
+      }
+    });
+  }
+  // onSubmit(form: NgForm){
+  //   console.log(form);
+  // }
+  onSubmit(){
+    this.submitted = true;
+    this.user.username = this.accessForm.value.userData.username;
+    this.user.email = this.accessForm.value.userData.email;
+    this.user.gender = this.accessForm.value.gender;
+    this.user.secretQuestion = this.accessForm.value.secret;
+    this.user.answer = this.accessForm.value.questionAnswer;
+    
+    this.accessForm.reset();
   }
 }
