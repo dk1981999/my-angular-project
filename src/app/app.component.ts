8f0b321d8f7a914442f1aab920b9a11e868534e8
DINESHKUMAR <dk1981999@gmail.com>
2021-06-09 19:43:10 +0530

    assignment7

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { CustomValidators } from './custom-validators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  //styleUrls: ['./app.component.css'],
-  styles:[`h3 {
-    color: red;
-  }`]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  serverElement = [{type: 'example', name: 'server'}]
-  serverName: string;
-  onServerCreated(serverData: {serverName: string}){
-  this.serverName = serverData.serverName;
+export class AppComponent implements OnInit {
+  projectForm: FormGroup;
+
+  ngOnInit(){
+    this.projectForm = new FormGroup({
+      'projectName': new FormControl(null,[Validators.required,CustomValidators.invalidProjectName],
+        CustomValidators.asyncInvalidProjectName),
+      'email': new FormControl(null,[Validators.required, Validators.email]),
+      'projectStatus': new FormControl('critical')
+    });
   }
+  onSubmit(){
+    console.log(this.projectForm.value)
+  }
+
 }
