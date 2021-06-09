e22472de03b2424fa2808ed64fa1392facc54aa6
DINESHKUMAR <dk1981999@gmail.com>
2021-06-09 10:15:36 +0530

    initial setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,15 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  //styleUrls: ['./app.component.css'],
-  styles:[`h3 {
-    color: red;
-  }`]
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  serverElement = [{type: 'example', name: 'server'}]
-  serverName: string;
-  onServerCreated(serverData: {serverName: string}){
-  this.serverName = serverData.serverName;
+  suggestUserName() {
+    const suggestedName = 'Superuser';
   }
 }
