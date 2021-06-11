5a3b9af02d37bf63166a04a5b457011daab3238c
DINESHKUMAR <dk1981999@gmail.com>
2021-06-11 11:20:15 +0530

    type

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { Post } from './posts.model';
 
 @Component({
   selector: 'app-root',
@@ -11,18 +13,42 @@ export class AppComponent implements OnInit {
 
   constructor(private http: HttpClient) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.onFetchData();
+  }
 
-  onCreatePost(postData: { title: string; content: string }) {
+  onCreatePost(postData: Post) {
     // Send Http request
-    console.log(postData);
+    this.http.post('https://my-angular-project-d1163-default-rtdb.firebaseio.com/posts.json',
+    postData).subscribe((responseData)=>{
+      console.log(responseData);
+    });
   }
 
   onFetchPosts() {
     // Send Http request
+    this.onFetchData();
   }
 
   onClearPosts() {
     // Send Http request
+  }
+
+  private onFetchData(){
+    this.http
+    .get('https://my-angular-project-d1163-default-rtdb.firebaseio.com/posts.json')
+    .pipe(
+      map((responseData: {[key: string]: Post }) =>{
+        const postsArray: Post[]=[];
+        for(const key in responseData){
+          if(responseData.hasOwnProperty(key)){
+            postsArray.push({...responseData[key], id:key });
+          }
+        }
+        return postsArray;
+      }))
+    .subscribe((responseData)=>{
+      console.log(responseData);
+    })
   }
 }
