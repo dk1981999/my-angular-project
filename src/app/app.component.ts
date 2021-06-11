10eac3eda01f9c7f3c624dbcdf576ab942a88b9a
DINESHKUMAR <dk1981999@gmail.com>
2021-06-11 13:24:29 +0530

    headers and params

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,54 +1,66 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 import { Post } from './posts.model';
+import { PostService } from './post.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  loadedPosts = [];
-
-  constructor(private http: HttpClient) {}
+export class AppComponent implements OnInit, OnDestroy {
+  loadedPosts: Post[] = [];
+  isFetching = false;
+  error = null;
+ private errorSub: Subscription;
+  constructor(private http: HttpClient,private postService: PostService) {}
 
   ngOnInit() {
-    this.onFetchData();
+    this.errorSub = this.postService.error.subscribe(errorMessage => {
+      this.error = errorMessage;
+    });
+    this.isFetching = true;
+    this.postService.fetchPosts().subscribe((responseData)=>{
+      this.isFetching = false;
+      this.loadedPosts = responseData;
+    },
+    error=>{
+      this.error = error.message;
+    });
   }
 
   onCreatePost(postData: Post) {
     // Send Http request
-    this.http.post('https://my-angular-project-d1163-default-rtdb.firebaseio.com/posts.json',
-    postData).subscribe((responseData)=>{
-      console.log(responseData);
-    });
+    this.postService.createAndStorePost(postData.title,postData.content);
+   
   }
 
   onFetchPosts() {
     // Send Http request
-    this.onFetchData();
+    this.isFetching = true;
+    this.postService.fetchPosts().subscribe((responseData)=>{
+      this.isFetching = false;
+      this.loadedPosts = responseData;
+    },
+    error=>{
+      this.isFetching = false;
+      this.error = error.error.error;
+      console.log(error.error.error)
+    });
   }
 
   onClearPosts() {
     // Send Http request
-  }
-
-  private onFetchData(){
-    this.http
-    .get('https://my-angular-project-d1163-default-rtdb.firebaseio.com/posts.json')
-    .pipe(
-      map((responseData: {[key: string]: Post }) =>{
-        const postsArray: Post[]=[];
-        for(const key in responseData){
-          if(responseData.hasOwnProperty(key)){
-            postsArray.push({...responseData[key], id:key });
-          }
-        }
-        return postsArray;
-      }))
-    .subscribe((responseData)=>{
-      console.log(responseData);
+    this.postService.deletePosts().subscribe(()=>{
+      this.loadedPosts = [];
     })
+  }
+  onHandleError(){
+    this.error = null;
+  }
+  ngOnDestroy(){
+    this.errorSub.unsubscribe();
   }
 }
---
+++ src/app/post.service.ts
@@ -0,0 +1,56 @@
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Post } from './posts.model';
+import { map,catchError } from 'rxjs/operators';
+import { Subject, throwError } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostService {
+
+  error = new Subject<string>();
+
+  constructor(private http: HttpClient) { }
+
+  createAndStorePost(title: string, content: string) {
+    const postData: Post = { title : title, content: content };
+    this.http.post<{name: string}>('https://my-angular-project-d1163-default-rtdb.firebaseio.com/posts.json',
+    postData).subscribe((responseData)=>{
+      console.log(responseData);
+    },
+    error =>{
+      this.error.next(error.message);
+    });
+  }
+
+  fetchPosts(){
+    let searchParams = new HttpParams;
+    searchParams = searchParams.append('id','true');
+    searchParams = searchParams.append('name','false');
+    return this.http
+    .get<{[key: string]: Post }>('https://my-angular-project-d1163-default-rtdb.firebaseio.com/posts.json',
+    {
+      headers: new HttpHeaders({'newHeaders':'Hello'}),
+      params: searchParams
+    })
+    .pipe(
+      map((responseData) =>{
+        const postsArray: Post[]=[];
+        for(const key in responseData){
+          if(responseData.hasOwnProperty(key)){
+            postsArray.push({...responseData[key], id:key });
+          }
+        }
+        return postsArray;
+      }),
+      catchError(errorRes => {
+        return throwError(errorRes);
+      })
+      );
+  }
+
+  deletePosts(){
+    return this.http.delete('https://my-angular-project-d1163-default-rtdb.firebaseio.com/posts.json');
+  }
+}
