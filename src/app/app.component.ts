555f1f37a48d93873a96d5dfa906f3049eb11b41
DINESHKUMAR <dk1981999@gmail.com>
2021-06-11 09:58:05 +0530

    initial setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  //styleUrls: ['./app.component.css'],
-  styles:[`h3 {
-    color: red;
-  }`]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  serverElement = [{type: 'example', name: 'server'}]
-  serverName: string;
-  onServerCreated(serverData: {serverName: string}){
-  this.serverName = serverData.serverName;
+export class AppComponent implements OnInit {
+  loadedPosts = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {}
+
+  onCreatePost(postData: { title: string; content: string }) {
+    // Send Http request
+    console.log(postData);
+  }
+
+  onFetchPosts() {
+    // Send Http request
+  }
+
+  onClearPosts() {
+    // Send Http request
   }
 }
